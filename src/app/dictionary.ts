0a14361711dda50c5f06f0f5b4b9f8cc77d20dd1
C1207435 <C1207435@ESBSA039903334.df.intrabb.bb.com.br>
2018-09-11 13:13:53 -0300

    Split Entry and Dictionary interfaces

--- src/app/dictionary.ts
+++ src/app/dictionary.ts
@@ -1,9 +1,11 @@
+export interface Entry {
+    'english': string,
+    'common': string,
+    'capitalized': string,
+    'allCaps': string
+}
+
 export interface Dictionary {
     'language': string,
-    'entries': Array<{
-        'english': string,
-        'common': string,
-        'capitalized': string,
-        'allCaps': string
-    }>
+    'entries': Array<Entry>
 }
