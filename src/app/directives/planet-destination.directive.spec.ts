46696855ea7c75ca5f20cb2ddaceb6929ed39bc0
Peter Shaws <inckie@gmail.com>
2018-09-18 19:22:17 -0300

    Missed file in the previous commit

---
+++ src/app/directives/planet-destination.directive.spec.ts
@@ -0,0 +1,8 @@
+import { PlanetDestinationDirective } from './planet-destination.directive';
+
+describe('PlanetDestinationDirective', () => {
+  it('should create an instance', () => {
+    const directive = new PlanetDestinationDirective();
+    expect(directive).toBeTruthy();
+  });
+});
